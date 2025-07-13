f9ae31dabc9000857af08155d6d422f2c0de5556
Athiruban8 <athiruban.p@gmail.com>
2025-07-13 18:08:31 +0530

    fix: build issue

--- app/(main)/transaction/page.jsx
+++ app/(main)/transaction/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { Suspense } from "react";
 import { getAllTransactions } from "@/actions/transaction";
 import { getAllAccounts } from "@/actions/account";
